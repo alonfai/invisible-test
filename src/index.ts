9ae93b61ac77403c47abde810638efdc88ffb130
Alon.Faingertz <alon.faingertz@service.vic.gov.au>
2020-08-09 21:09:49 +1000

    resolve build script module resolution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+require('module-alias/register');
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 if (process.argv.length < 3) {
-  console.error('Missing npm script name to execute and/or the file name as source ( ex: yarn start input.txt )');
+  console.error(
+    'Missing npm script name to execute and/or the file name as source ( ex: yarn start input.txt )'
+  );
   process.exit(1);
 }
 
